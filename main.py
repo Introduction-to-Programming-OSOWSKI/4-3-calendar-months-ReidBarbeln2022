8354c070873adc9e506b4bd0d84f32b2cd096758
ReidBarbeln2022 <814684@mystma.org>
2021-02-18 15:55:25 +0000

    calender try 1

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-#WRITE YOUR CODE IN THIS FILE
+def calendar(m):
+    date = "january", "february", "march", "april", "may", "june", "july", "august", "september", "october", "november", "december"
+
+    for i in range (0, len(date)):
+        if m == date[i]:
+            return i + 1
+    return m + " is not a munth"
+
+print (calendar("march"))
